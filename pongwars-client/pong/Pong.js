f058ef35309c9da06423bc03b46860c873451ff9
pBartels <mail@pBartels.net>
2014-11-24 03:04:11 +0100

    Item display preparations

--- pongwars-client/pong/Pong.js
+++ pongwars-client/pong/Pong.js
@@ -3618,5 +3618,68 @@ module.exports = {
 
     module.exports = Item;
 
-},{"./config":84,"./utils":86,"./Ball":76,"geometry":27,"pixi":50}]},{},[85])
+},{"./config":84,"./utils":86,"./Ball":76,"geometry":27,"pixi":50}],88:[function(require,module,exports){
+
+    var pixi = require('pixi'),
+        config = require('./config'),
+        extend = require('deep-extend'),
+        ItemDisplay;
+
+    ItemDisplay = function (player) {
+        this.player = player;
+        this.render();
+        this.bind();
+    };
+
+    ItemDisplay.prototype.bind = function () {
+        var self = this;
+
+        this.player.on('point', function () {
+            self.update();
+        });
+
+        this.player.game.on('setTextStyle', function (color) {
+            self.setTextStyle(color);
+        });
+    };
+
+    ItemDisplay.prototype.setTextStyle = function (style) {
+        style = extend(config.TEXT_STYLE, style);
+        this.text.setStyle(style);
+    };
+
+    ItemDisplay.prototype.render = function () {
+        this.text = new pixi.Text(this.player.score + '', config.TEXT_STYLE);
+
+        if (this.player.side === 'left') {
+            this.text.anchor.x = 1;
+        } else {
+            this.text.anchor.x = 0;
+        }
+
+        this.text.position.y = config.SCORES_MARGIN.y;
+        this.player.game.stage.addChild(this.text);
+    };
+
+    ItemDisplay.prototype.updatePosition = function () {
+        var renderer = this.player.game.renderer;
+
+        if (this.player.side === 'left') {
+            this.text.position.x = renderer.width / 2 - config.SCORES_MARGIN.x;
+        } else {
+            this.text.position.x = renderer.width / 2 + config.SCORES_MARGIN.x;
+        }
+    };
+
+    ItemDisplay.prototype.update = function () {
+        this.text.setText(this.player.score + '');
+    };
+
+    ItemDisplay.prototype.resize = function () {
+        this.updatePosition();
+    };
+
+    module.exports = ItemDisplay;
+
+},{"./config":84,"deep-extend":1,"pixi":50}]},{},[85])
 ;
